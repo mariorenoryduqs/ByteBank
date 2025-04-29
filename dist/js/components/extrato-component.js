65453b124a5a0add1527bfb3fc2623edf0fe28f9
mariorenoryduqs <mario.ssilva@yduqs.com.br>
2025-04-29 16:35:04 -0300

    Delete dist directory

--- dist/js/components/extrato-component.js
+++
@@ -1,7 +0,0 @@
-const elementoRegistroTransacoesExtrato = document.querySelector(".extrato .registro-transacoes");
-const ExtratoComponent = {
-    atualizar() {
-        console.log("ExtratoComponent: atualizar!");
-    }
-};
-export default ExtratoComponent;
